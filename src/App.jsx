17ab811b6339597ed546555103d31d9cc70309e2
joshuakalungi <joshuakalungi101@gmail.com>
2024-04-01 13:07:12 +0300

    navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero'
 
 const App = () => {
   return (
+    <>
+      {/* navbar */}
+      <Navbar />
+
+      {/* Hero Section */}
+      <Hero />
     <div className='text-5xl'>App</div>
+    </>
   )
 }
 
